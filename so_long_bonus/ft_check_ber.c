3c23b0725a41b8a7522c6cc2df669a5d97789cb8
Houssame Ait hsain <hossame@inbox.ru>
2023-01-12 12:43:37 +0100

    12:43

---
+++ so_long_bonus/ft_check_ber.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_ber.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hait-hsa <hait-hsa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 20:17:16 by hait-hsa          #+#    #+#             */
+/*   Updated: 2023/01/02 13:56:46 by hait-hsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_check_map_name(char *tmp)
+{
+	char	*arr;
+	int		i;
+	int		j;
+
+	arr = ".ber";
+	j = 3;
+	i = ft_strlen(tmp);
+	i--;
+	while (j >= 0)
+	{
+		if (tmp[i] != arr[j])
+		{
+			ft_printf("INVALID MAP NAME!");
+			exit(0);
+		}
+		j--;
+		i--;
+	}
+}
+
+void	ft_check_ber(t_so_long1 *var, int arc, char *tmp)
+{
+	if (arc == 2 && tmp)
+	{
+		ft_check_map_name(tmp);
+		new_map(var, tmp);
+	}
+	else
+	{
+		ft_printf("MAP NOT FOUND!");
+		exit(0);
+	}
+}
